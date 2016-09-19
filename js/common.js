728f701cfc3c57f6c72f97693449e925544a5e65
Vasil Korkin <v.ivan.korkin@gmail.com>
2016-09-19 16:32:40 +0400

    [ js ] Отключение плагина lightslider, карусель реализована на owl

--- js/common.js
+++ js/common.js
@@ -10,11 +10,30 @@ $(function() {
  	stopOnHover: true
  });
 
+
+$(".carusel").owlCarousel({
+ 	items : 1,
+ 	slideSpeed: 1000,
+ 	autoPlay: true,
+ 	itemsDesktop : [1199,1],
+ 	itemsDesktopSmall : [980,1],
+ 	itemsTablet: [768,1],
+ 	stopOnHover: true
+ });
+ 
+
 $(".custom_prev").click(function() {
      $("#carusel_photo_report").trigger("owl.prev");
   });
   $(".custom_next").click(function() {
      $("#carusel_photo_report").trigger("owl.next");
+  });
+
+$(".custom_prev_banner").click(function() {
+     $(".carusel").trigger("owl.prev");
+  });
+  $(".custom_next_banner").click(function() {
+     $(".carusel").trigger("owl.next");
   });
 
 
