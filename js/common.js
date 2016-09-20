086cdd6dbe493415e36fb3e8f02d90855246c55d
Vasil Korkin <v.ivan.korkin@gmail.com>
2016-09-20 12:11:33 +0400

    [ js, css, html ] Верстка страницы списка новостей
    Нужно проверить адаптивность, протестировать grid bootstrap

--- js/common.js
+++ js/common.js
@@ -40,6 +40,8 @@ $(".custom_prev_banner").click(function() {
 // Вычисление максимальной высоты колонок Афиша и Новости, меньшей колонке присваиваем большую высоту
 var heght_poster = $(".poster").css('height');
 var heght_news = $(".news").css('height');
+if($.trim(heght_poster) != '' && $.trim(heght_news) != '' &&  document.documentElement.clientWidth > 768){
+
 heght_poster = Number(heght_poster.substr(0, heght_poster.length - 2)) - 105;
 heght_news = Number(heght_news.substr(0, heght_news.length - 2)) - 105;
 
@@ -51,6 +53,26 @@ if( heght_poster > heght_news){
 	$(".poster").css("padding-bottom","0");
 	$(".poster").height(heght_news + 105);
 	// console.log(heght_news);
+}
+}
+
+// Вычисление максимальной высоты колонок на странице новости, меньшей колонке присваиваем большую высоту
+ var heght_img_news = $(".row_news .img_news").css('height');
+ var heght_text_news = $(".row_news .text_news").css('height');
+
+if($.trim(heght_img_news) != '' && $.trim(heght_text_news) != '' && document.documentElement.clientWidth > 768){
+
+ heght_img_news = Number(heght_img_news.substr(0, heght_img_news.length - 2));
+ heght_text_news = Number(heght_text_news.substr(0, heght_text_news.length - 2));
+ if( heght_img_news > heght_text_news){
+   $(".row_news .text_news").height(heght_img_news );
+   // $(".news").css("padding-bottom","0");
+   // console.log(heght_img_news);
+ }else{
+   // $(".row_news .img_news").css("padding-bottom","0");
+   $(".row_news .img_news").height(heght_text_news);
+   // console.log(heght_text_news);
+}
 }
 
 });
